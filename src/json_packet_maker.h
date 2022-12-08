b1c35282dfb6fe416d79f10e5b558d67fa10905d
Doubb <doubele112@naver.com>
2022-12-08 16:12:12 +0900

    Make RRC/NAS packet from JSON

---
+++ src/json_packet_maker.h
@@ -0,0 +1,24 @@
+#ifndef __JSON_PACKET_MAKER__
+#define __JSON_PACKET_MAKER__
+
+#include <iostream>
+#include <unistd.h>
+#include <string>
+
+#include "mitm_lib/asn1/asn1_utils.h"
+
+namespace jsonPacketMaker {
+  uint8_t* json_to_packet(std::string buf, uint8_t* original_msg, int size);
+
+  // RRC
+  void handle_rrc_security_mode_complete(uint8_t* original_msg, int rrcTransactionIdentifier, int size);
+  void handle_rrc_security_mode_command(uint8_t* original_msg, int rrcTransactionIdentifier, std::string cipheringAlgorithm, std::string integrityAlgorithm, bool non_crit_ext_present, std::string late_non_crit_ext, int size);
+  void handle_rrc_reject(uint8_t* original_msg, uint8_t waitTime, int size);
+  void handle_rrc_ue_cap_enquiry(uint8_t* original_msg, int rrcTransactionIdentifier, std::string ratType, std::string capReqFilter, int size);
+  //void handle_rrc_reject(uint8_t waitTime, int size);
+  
+  // NAS
+  void handle_nas_security_mode_command(uint8_t* original_msg, int rrcTransactionIdentifier, uint8_t* MAC, int sn, int size);
+}
+
+#endif
